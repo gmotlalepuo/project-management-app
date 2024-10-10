17c28bcd4b10000b24ec7ada771c2c1c532e343c
Stefan Gogov <stefangogov@proton.me>
2024-10-10 20:30:57 +0300

    Add Project and Task models, factories, and migrations
    This commit adds the Project and Task models, along with their respective factories and migrations. The Project model has a one-to-many relationship with the Task model. The models use the HasFactory trait for easy factory creation. The migrations create the necessary database tables for the models. This commit also includes a seeder that creates a test user and generates 30 projects, each with 30 tasks.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,23 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Stefan',
+            'email' => 'stefan@example.com',
+            'password' => bcrypt('password1'),
+            'email_verified_at' => time(),
         ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
     }
 }
