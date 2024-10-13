64cdec6f35daaa236fc2ab6e8b2d92a73d861d13
stekatag <stefangogov@proton.me>
2024-10-13 18:30:17 +0300

    Update .prettierrc to remove "singleQuote" option

--- resources/js/Pages/Project/Index.tsx
+++ resources/js/Pages/Project/Index.tsx
@@ -1,23 +1,36 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, usePage } from '@inertiajs/react';
+import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Project } from "@/types";
+import { Head, Link, router } from "@inertiajs/react";
 
-type Project = {
-  projects: object[];
-  data: Array<{
-    id: number;
-    name: string;
-    description: string;
-    created_at: string;
-    due_date: string;
-    status: string;
-    image_path: string;
-    createdBy: Object;
-    updatedBy: Object;
-  }>;
+type IndexProps = {
+  projects: Project;
+  queryParams: { [key: string]: any } | null;
 };
 
-export default function Index({ projects }: { projects: Project }) {
-  const user = usePage().props.auth.user;
+export default function Index({ projects, queryParams }: IndexProps) {
+  queryParams = queryParams || {};
+  const searchFieldChanged = (name: string, value: string) => {
+    if (value) {
+      queryParams[name] = value;
+    } else {
+      delete queryParams[name];
+    }
+
+    router.get(route("project.index"), queryParams, { preserveState: true });
+  };
+
+  const onKeyDown = (
+    name: string,
+    e: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
+    if (e.key !== "Enter") return;
+
+    searchFieldChanged(name, e.currentTarget.value);
+  };
 
   return (
     <AuthenticatedLayout
@@ -31,29 +44,104 @@ export default function Index({ projects }: { projects: Project }) {
 
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+          <div className="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+              <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
                 <thead className="border-b-2 border-gray-500 bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
                   <tr className="text-nowrap">
-                    <th className="px-3 py-2">ID</th>
-                    <th className="px-3 py-2">Image</th>
-                    <th className="px-3 py-2">Name</th>
-                    <th className="px-3 py-2">Status</th>
-                    <th className="px-3 py-2">Create Date</th>
-                    <th className="px-3 py-2">Due Date</th>
-                    <th className="px-3 py-2">Created By</th>
-                    <th className="px-3 py-2">Actions</th>
+                    <th className="px-3 py-3">ID</th>
+                    <th className="px-3 py-3">Image</th>
+                    <th className="px-3 py-3">Name</th>
+                    <th className="px-3 py-3">Status</th>
+                    <th className="px-3 py-3">Create Date</th>
+                    <th className="px-3 py-3">Due Date</th>
+                    <th className="px-3 py-3">Created By</th>
+                    <th className="px-3 py-3 text-right">Actions</th>
+                  </tr>
+                </thead>
+                <thead className="border-b-2 border-gray-500 bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput
+                        className="w-full"
+                        defaultValue={queryParams.name}
+                        placeholder="Project Name"
+                        onBlur={(e) =>
+                          searchFieldChanged("name", e.target.value)
+                        }
+                        onKeyDown={(e) => onKeyDown("name", e)}
+                      />
+                    </th>
+                    <th className="px-3 py-3">
+                      <SelectInput
+                        className="w-full"
+                        defaultValue={queryParams.status}
+                        onChange={(e) =>
+                          searchFieldChanged("status", e.target.value)
+                        }
+                      >
+                        <option value="">Select Status</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </SelectInput>
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3 text-right"></th>
                   </tr>
                 </thead>
                 <tbody>
                   {projects.data.map((project) => (
-                    <tr className="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <tr
+                      className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
+                      key={project.id}
+                    >
                       <td className="px-3 py-2">{project.id}</td>
+                      <td className="px-3 py-2">
+                        <img
+                          src={project.image_path}
+                          style={{ width: 100 }}
+                          alt={project.name}
+                        />
+                      </td>
+                      <td className="px-3 py-2">{project.name}</td>
+                      <td className="px-3 py-2">
+                        <span
+                          className={`rounded px-2 py-1 text-white ${PROJECT_STATUS_CLASS_MAP[project.status]}`}
+                        >
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
+                      <td className="text-nowrap px-3 py-2">
+                        {project.created_at}
+                      </td>
+                      <td className="text-nowrap px-3 py-2">
+                        {project.due_date}
+                      </td>
+                      <td className="px-3 py-2">{project.createdBy.name}</td>
+                      <td className="px-3 py-2">
+                        <Link
+                          href={route("project.edit", project.id)}
+                          className="mx-1 font-medium text-blue-600 hover:underline dark:text-blue-500"
+                        >
+                          Edit
+                        </Link>
+                        <Link
+                          href={route("project.destroy", project.id)}
+                          className="mx-1 font-medium text-red-600 hover:underline dark:text-red-500"
+                        >
+                          Delete
+                        </Link>
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
+              <Pagination links={projects.meta.links} />
             </div>
           </div>
         </div>
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,14 +1,56 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at?: string;
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at?: string;
 }
 
 export type PageProps<
-    T extends Record<string, unknown> = Record<string, unknown>,
+  T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {
-    auth: {
-        user: User;
+  auth: {
+    user: User;
+  };
+};
+
+export type Project = {
+  data: Array<{
+    id: number;
+    name: string;
+    description: string;
+    created_at: string;
+    due_date: string;
+    status: 'completed' | 'pending' | 'in_progress';
+    image_path: string;
+    createdBy: {
+      id: number;
+      name: string;
+      email: string;
     };
+    updatedBy: {
+      id: number;
+      name: string;
+      email: string;
+    };
+  }>;
+  links: {
+    first: string | null;
+    last: string | null;
+    prev: string | null;
+    next: string | null;
+  };
+  meta: {
+    current_page: number;
+    from: number;
+    last_page: number;
+    links: Array<{
+      url: string | null;
+      label: string;
+      active: boolean;
+    }>;
+    path: string;
+    per_page: number;
+    to: number;
+    total: number;
+  };
 };
