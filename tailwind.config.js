77a74be9a9e65748ba5430b7ea3738e092748dae
stekatag <stefangogov@proton.me>
2024-10-20 20:22:59 +0300

    refactor: Update base radius in app.css and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
+  darkMode: ["class"],
   content: [
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     "./storage/framework/views/*.php",
@@ -13,12 +13,18 @@ export default {
   ],
 
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: ["Figtree", ...defaultTheme.fontFamily.sans]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
 
-  plugins: [forms],
+  plugins: [forms, require("tailwindcss-animate")],
 };
